257fb3a608bd503b1c5a806e2b1a87c5a27bd4af
lmeng25 <lmeng2@seattleu.edu>
2024-02-24 21:50:51 -0800

    add order payment api

---
+++ app/api/orders/[id]/payment/route.ts
@@ -0,0 +1,38 @@
+import { auth } from '@/lib/auth';
+import mongodb from '@/lib/mongodb';
+import OrderModel from '@/lib/models/OrderModel';
+
+export const POST = auth(async (...request: any) => {
+    const [req, { params }] = request;
+    if (!req.auth) {
+        return Response.json(
+            { message: 'unauthorized' },
+            {
+                status: 401,
+            }
+        );
+    }
+    await mongodb();
+    const order = await OrderModel.findById(params.id);
+    if (order) {
+        try {
+            order.isPaid = true;
+            const updatedOrder = await order.save();
+            return Response.json(updatedOrder);
+        } catch (err: any) {
+            return Response.json(
+                { message: err.message },
+                {
+                    status: 500,
+                }
+            );
+        }
+    } else {
+        return Response.json(
+            { message: 'Order not found' },
+            {
+                status: 404,
+            }
+        );
+    }
+}) as any;
