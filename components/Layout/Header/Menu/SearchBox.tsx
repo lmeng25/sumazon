1d9a10378b34d553aa2f5116f4c0d9ea4fe07c89
lmeng25 <lmeng2@seattleu.edu>
2024-02-27 21:28:45 -0800

    add searchBox component

---
+++ components/Layout/Header/Menu/SearchBox.tsx
@@ -0,0 +1,15 @@
+'use client';
+import { useSearchParams } from 'next/navigation';
+
+export const SearchBox = () => {
+    const searchParams = useSearchParams();
+    const name = searchParams.get('name') || '';
+    return (
+        <form action="/search" method="GET">
+            <div className="join">
+                <input placeholder="Search" defaultValue={name} name="name" />
+                <button>Search</button>
+            </div>
+        </form>
+    );
+};
