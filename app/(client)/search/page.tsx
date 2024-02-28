249eb079f8605317b067d7c173bcae6b82ccf0f7
lmeng25 <lmeng2@seattleu.edu>
2024-02-27 21:29:11 -0800

    add search page

---
+++ app/(client)/search/page.tsx
@@ -0,0 +1,54 @@
+import productServices from '@/lib/ProductFilter';
+import Image from 'next/image';
+import Link from 'next/link';
+import './SearchPage.css';
+
+export default async function SearchPage({
+    searchParams: { name = '' },
+}: {
+    searchParams: { name: string };
+}) {
+    let searchResult = <h2>No matching product</h2>;
+
+    const products = await productServices.getByName(name);
+    if (products && products.length > 0) {
+        searchResult = (
+            <div className="productListContainer">
+                {products.map((product) => (
+                    <div key={product.slug} className="productItem">
+                        <h2>{product.name}</h2>
+                        <div>{product.brand}</div>
+                        <div>
+                            <Link href={`/product/${product.slug}`}>
+                                <Image
+                                    src={product.image}
+                                    alt={product.name}
+                                    width={200}
+                                    height={200}
+                                />
+                            </Link>
+                        </div>
+                        <div className="productPrice">{`$${product.price}`}</div>{' '}
+                        <div>Category: {product.category}</div>
+                        <div>{product.description}</div>
+                        <div
+                            className="stockAvailability"
+                            style={{
+                                color:
+                                    product.quantity === 0 ? 'red' : 'inherit',
+                            }}
+                        >
+                            Available in stock: {product.quantity}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <div>{searchResult}</div>
+        </div>
+    );
+}
