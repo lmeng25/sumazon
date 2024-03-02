e5b102efedf84aa99ca9eb0333154363b20f0798
lmeng25 <lmeng2@seattleu.edu>
2024-03-01 22:48:19 -0800

    fix bugs in order POST api

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,7 +1,5 @@
 import { auth } from '@/lib/auth';
-import mongodb from '@/lib/mongodb';
 import OrderModel, { OrderItem } from '@/lib/models/OrderModel';
-import ProductModel from '@/lib/models/ProductModel';
 import { roundToTwo } from '@/lib/utils';
 
 export const POST = auth(async (request: any) => {
@@ -16,29 +14,23 @@ export const POST = auth(async (request: any) => {
     const { user } = request.auth;
     try {
         const payload = await request.json();
-        await mongodb();
-        const dbProductPrices = await ProductModel.find(
-            {
-                _id: { $in: payload.items.map((x: { _id: string }) => x._id) },
-            },
-            'price'
-        );
-        const dbOrderItems = payload.items.map((x: { _id: string }) => ({
-            ...x,
-            product: x._id,
-            price: dbProductPrices.find((x) => x._id === x._id).price,
-            _id: undefined,
-        }));
-
+        const orderItems = payload.items.map((x: any) => {
+            return {
+                ...x,
+                product: x._id,
+                price: x.price,
+                _id: undefined,
+            };
+        });
         const totalPrice = roundToTwo(
-            dbOrderItems.reduce(
+            orderItems.reduce(
                 (acc: any, item: any) => acc + item.price * item.quantity,
                 0
             )
         );
 
         const newOrder = new OrderModel({
-            items: dbOrderItems,
+            items: orderItems,
             totalPrice,
             shippingAddress: payload.shippingAddress,
             user: user._id,
