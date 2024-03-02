803c7f7d0c550a1dff458d2f380ec9c6947efa28
lmeng25 <lmeng2@seattleu.edu>
2024-03-01 22:02:38 -0800

    update styles in order details

--- app/(client)/checkout/checkoutForm.tsx
+++ app/(client)/checkout/checkoutForm.tsx
@@ -47,22 +47,67 @@ const CheckoutForm = () => {
         }
     };
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor="address">Shipping Address</label>
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            style={{
+                maxWidth: '500px',
+                margin: 'auto',
+                padding: '20px',
+                border: '1px solid #ddd',
+                borderRadius: '5px',
+                boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+            }}
+        >
+            <div style={{ marginBottom: '10px' }}>
+                <h1
+                    style={{
+                        display: 'block',
+                        marginBottom: '5px',
+                        textAlign: 'center',
+                    }}
+                >
+                    Shipping Address
+                </h1>
+                <br />
                 <input
                     {...register('address', {
                         required: 'Shipping address is required',
                     })}
                     id="address"
                     type="text"
+                    style={{
+                        width: '100%',
+                        padding: '8px',
+                        border: '1px solid #ddd',
+                        borderRadius: '4px',
+                    }}
                 />
             </div>
-            <div>
+            <br />
+            <div
+                style={{
+                    marginBottom: '20px',
+                    fontSize: '18px',
+                    fontWeight: 'bold',
+                }}
+            >
                 <strong>Total:</strong> $
                 {items.reduce((acc, i) => acc + i.price * i.quantity, 0)}
             </div>
-            <button>Submit Order</button>
+            <button
+                style={{
+                    width: '30%',
+                    padding: '10px',
+                    backgroundColor: '#007bff',
+                    color: 'white',
+                    border: 'none',
+                    borderRadius: '4px',
+                    cursor: 'pointer',
+                    fontSize: '16px',
+                }}
+            >
+                Submit Order
+            </button>
         </form>
     );
 };
