c66c7c81a80b53cae679d26c932e8df11444afcc
lmeng25 <lmeng2@seattleu.edu>
2024-03-01 20:32:05 -0800

    add header styles

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -1,18 +1,33 @@
 import Link from 'next/link';
 import Menu from './Menu/Menu';
 import { SearchBox } from './Menu/SearchBox';
+import Image from 'next/image';
+import './Header.css';
 
 export default function Header() {
     return (
         <header>
             <nav>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <Link href="/">SUMAZON</Link>
+                <div
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '50px',
+                    }}
+                >
+                    <Link href="/">
+                        <Image
+                            src="/logo.svg"
+                            alt="logo"
+                            width={150}
+                            height={100}
+                        />
+                    </Link>
+                    <SearchBox />
+
                     <Menu />
                 </div>
-                <div>
-                    <SearchBox />
-                </div>
+                <div></div>
             </nav>
         </header>
     );
