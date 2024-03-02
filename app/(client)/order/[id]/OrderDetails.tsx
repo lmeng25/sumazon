6a79e56afdac055972e25e61412dba90053e6b20
lmeng25 <lmeng2@seattleu.edu>
2024-03-01 22:24:44 -0800

    update table layout in order details page

--- app/(client)/order/[id]/OrderDetails.tsx
+++ app/(client)/order/[id]/OrderDetails.tsx
@@ -24,30 +24,165 @@ export default function OrderDetails({ orderId }: { orderId: string }) {
     };
 
     return (
-        <div>
-            <h1>Order Details</h1>
-            <p>Order Total: ${data.totalPrice}</p>
-            <p>Shipping Address: {data.shippingAddress}</p>
-            <h2>Items</h2>
-            <ul>
-                {data.items.map((item: OrderItem) => (
-                    <li key={item.slug}>
-                        <Link href={`/product/${item.slug}`}>
-                            <Image
-                                src={item.image}
-                                alt={item.name}
-                                width={50}
-                                height={50}
-                            />
-                            {item.name}
-                            {item.price}
-                            {item.quantity}
-                        </Link>
-                    </li>
-                ))}
+        <div
+            style={{
+                maxWidth: '600px',
+                margin: 'auto',
+                padding: '20px',
+                textAlign: 'center',
+            }}
+        >
+            <h1
+                style={{
+                    borderBottom: '2px solid #ddd',
+                    paddingBottom: '10px',
+                }}
+            >
+                Order Details
+            </h1>
+            <p style={{ fontSize: '18px', margin: '10px 0' }}>
+                Order Total: <strong>${data.totalPrice}</strong>
+            </p>
+            <p style={{ fontSize: '18px', margin: '10px 0' }}>
+                Shipping Address: <strong>{data.shippingAddress}</strong>
+            </p>
+            <h2
+                style={{
+                    borderTop: '1px solid #ddd',
+                    paddingTop: '10px',
+                    marginTop: '20px',
+                }}
+            >
+                Items
+            </h2>
+            <ul style={{ listStyle: 'none', padding: '0', textAlign: 'left' }}>
+                <table
+                    style={{
+                        width: '100%',
+                        borderCollapse: 'collapse',
+                    }}
+                >
+                    <thead>
+                        <tr>
+                            <th
+                                style={{
+                                    border: '1px solid #ddd',
+                                    padding: '8px',
+                                    textAlign: 'center',
+                                }}
+                            >
+                                Product
+                            </th>
+                            <th
+                                style={{
+                                    border: '1px solid #ddd',
+                                    padding: '8px',
+                                    textAlign: 'center',
+                                }}
+                            >
+                                Price
+                            </th>
+                            <th
+                                style={{
+                                    border: '1px solid #ddd',
+                                    padding: '8px',
+                                    textAlign: 'center',
+                                }}
+                            >
+                                Quantity
+                            </th>
+                            <th
+                                style={{
+                                    border: '1px solid #ddd',
+                                    padding: '8px',
+                                    textAlign: 'center',
+                                }}
+                            >
+                                Subtotal
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.items.map((item: OrderItem) => (
+                            <tr key={item.slug}>
+                                <td
+                                    style={{
+                                        border: '1px solid #ddd',
+                                        padding: '8px',
+                                    }}
+                                >
+                                    <Link
+                                        href={`/product/${item.slug}`}
+                                        style={{
+                                            display: 'flex',
+                                            alignItems: 'center',
+                                            textDecoration: 'none',
+                                            color: 'inherit',
+                                        }}
+                                    >
+                                        <Image
+                                            src={item.image}
+                                            alt={item.name}
+                                            width={50}
+                                            height={50}
+                                        />
+                                        <span
+                                            style={{
+                                                marginLeft: '10px',
+                                                textAlign: 'center',
+                                            }}
+                                        >
+                                            {item.name}
+                                        </span>
+                                    </Link>
+                                </td>
+                                <td
+                                    style={{
+                                        border: '1px solid #ddd',
+                                        padding: '8px',
+                                        textAlign: 'center',
+                                    }}
+                                >
+                                    ${item.price}
+                                </td>
+                                <td
+                                    style={{
+                                        border: '1px solid #ddd',
+                                        padding: '8px',
+                                        textAlign: 'center',
+                                    }}
+                                >
+                                    {item.quantity}
+                                </td>
+                                <td
+                                    style={{
+                                        border: '1px solid #ddd',
+                                        padding: '8px',
+                                        textAlign: 'center',
+                                    }}
+                                >
+                                    ${item.quantity * item.price}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </ul>
 
-            <button onClick={handlePayment} disabled={data.isPaid}>
+            <button
+                onClick={handlePayment}
+                disabled={data.isPaid}
+                style={{
+                    padding: '10px 20px',
+                    backgroundColor: data.isPaid ? '#cccccc' : '#007bff',
+                    color: 'white',
+                    border: 'none',
+                    borderRadius: '4px',
+                    cursor: data.isPaid ? 'default' : 'pointer',
+                    fontSize: '16px',
+                    marginTop: '20px',
+                }}
+            >
                 {data.isPaid ? 'Order Paid' : 'Pay Now'}
             </button>
         </div>
