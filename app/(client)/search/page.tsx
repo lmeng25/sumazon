cb526cf3424687314fba916844316df72aa50ccb
lmeng25 <lmeng2@seattleu.edu>
2024-03-02 21:24:19 -0800

    update product search page layout

--- app/(client)/search/page.tsx
+++ app/(client)/search/page.tsx
@@ -8,39 +8,49 @@ export default async function SearchPage({
 }: {
     searchParams: { name: string };
 }) {
-    let searchResult = <h2>No matching product</h2>;
-
+    let searchResult = <h1 className="productTitle">No Matching Product</h1>;
     const products = await productServices.getByName(name);
     if (products && products.length > 0) {
         searchResult = (
-            <div className="productListContainer">
-                {products.map((product) => (
-                    <div key={product.slug} className="productItem">
-                        <h2>{product.name}</h2>
-                        <div>
-                            <Link href={`/product/${product.slug}`}>
-                                <Image
-                                    src={product.image}
-                                    alt={product.name}
-                                    width={200}
-                                    height={200}
-                                />
-                            </Link>
+            <div>
+                <h1 className="productTitle">
+                    {products.length}{' '}
+                    {products.length === 1 ? 'Product' : 'Products'} Found For
+                    {' "'}
+                    {name}
+                    {'" '}
+                </h1>
+                <div className="productListContainer">
+                    {products.map((product) => (
+                        <div key={product.slug} className="productItem">
+                            <h2>{product.name}</h2>
+                            <div>
+                                <Link href={`/product/${product.slug}`}>
+                                    <Image
+                                        src={product.image}
+                                        alt={product.name}
+                                        width={200}
+                                        height={200}
+                                    />
+                                </Link>
+                            </div>
+                            <div className="productPrice">{`$${product.price}`}</div>{' '}
+                            <div
+                                className="stockAvailability"
+                                style={{
+                                    color:
+                                        product.quantity === 0
+                                            ? 'red'
+                                            : 'inherit',
+                                }}
+                            >
+                                {product.quantity === 0
+                                    ? 'Out of stock'
+                                    : 'In stock'}
+                            </div>
                         </div>
-                        <div className="productPrice">{`$${product.price}`}</div>{' '}
-                        <div
-                            className="stockAvailability"
-                            style={{
-                                color:
-                                    product.quantity === 0 ? 'red' : 'inherit',
-                            }}
-                        >
-                            {product.quantity === 0
-                                ? 'Out of stock'
-                                : 'In stock'}
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         );
     }
