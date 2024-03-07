21c64ce90dfc98a543e5dd0c591b41602265c41b
lmeng25 <lmeng2@seattleu.edu>
2024-03-06 18:51:44 -0800

    update product page

--- app/(client)/product/[slug]/page.tsx
+++ app/(client)/product/[slug]/page.tsx
@@ -2,18 +2,6 @@ import ProductCard from '@/components/Product/ProductCard/ProductCard';
 import productFilter from '@/lib/ProductFilter';
 import Link from 'next/link';
 
-export async function Metadata(props: any) {
-    const slug = props.params.slug;
-    const product = await productFilter.getBySlug(slug);
-    if (!product) {
-        return { title: 'Oops, product not found' };
-    }
-    return {
-        title: product.name,
-        description: product.description,
-    };
-}
-
 export default async function ProductDetails(props: any) {
     const slug = props.params.slug;
     const product = await productFilter.getBySlug(slug);
